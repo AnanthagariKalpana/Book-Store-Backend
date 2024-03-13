cb4872e9507948fd15cb14bae3f902eb78303fae
kalpana <kalpanas1929@gmail.com>
2024-03-13 12:34:23 +0530

    created book model

---
+++ src/models/book.model.js
@@ -0,0 +1,35 @@
+import { Schema, model } from 'mongoose';
+
+const bookSchema = new Schema(
+    {
+        bookName: {
+            type: String
+        },
+        author: {
+            type: String
+        },
+        description: {
+            type: String
+        },
+        quantity: {
+            type: Number
+        },
+        price: {
+            type: Number
+        },
+        discountPrice: {
+            type: Number
+        },
+        admin_user_id: {
+            type: String
+        },
+        bookImage: {
+            type: String
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+export default model('User', bookSchema);
