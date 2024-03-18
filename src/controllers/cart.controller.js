69c76d59f065d9e9233a8285f12c994988eaf91e
kalpana <kalpanas1929@gmail.com>
2024-03-18 14:28:46 +0530

    [kalpana]:modified logics

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -6,8 +6,9 @@ import * as CartService from '../services/cart.service';
 export const addToCart = async (req, res, next) => {
   try {
     //console.log(req.params._id);
-    const data = await CartService.addToCart(req.params._id,req.body.userID);
-    console.log(data);
+    console.log(req.user.id);
+    const data = await CartService.addToCart(req.params._id,req.user.id);
+    //console.log(data);
     res.status(HttpStatus.OK).json({
       code: HttpStatus.OK,
       data: data,
@@ -17,3 +18,62 @@ export const addToCart = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getCart = async (req, res, next) => {
+    try {
+      //console.log(req.body.userID);
+      const data = await CartService.getCart(req.user.id);
+      //console.log(data);
+      res.status(HttpStatus.OK).json({
+        code: HttpStatus.OK,
+        data: data,
+        message: 'Cart fetched successfully'
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  export const deleteCart = async (req, res, next) => {
+    try {
+      //console.log("controller....",req.user.userID);
+      const data = await CartService.deleteCart(req.user.userID);
+      //console.log(data);
+      res.status(HttpStatus.OK).json({
+        code: HttpStatus.OK,
+        data: data,
+        message: 'Cart deleted successfully'
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  export const removeCartItem = async (req, res, next) => {
+    try {
+      //console.log(req.params._id);
+      const data = await CartService.removeCartItem(req.params._id,req.user.id);
+      //console.log(data);
+      res.status(HttpStatus.OK).json({
+        code: HttpStatus.OK,
+        data: data,
+        message: 'Cart removed successfully'
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  export const isPurchase = async (req, res, next) => {
+    try {
+      const data = await CartService.isPurchase(req.user.id);
+      //console.log(data);
+      res.status(HttpStatus.OK).json({
+        code: HttpStatus.OK,
+        data: data,
+        message: 'successfully'
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
